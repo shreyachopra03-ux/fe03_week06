507ef16430bd01e2738a9c8dc98de93bb6ec22cf
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-18 21:12:04 +0530

    docs: Block, block scope

---
+++ BlockScopeAndShadowing.ts
@@ -0,0 +1,13 @@
+// Block is also known as Compound statement.
+// We use multiple statements in a block together where js expects only 1 statement.
+// For combining mutiple statements together, always use a block, multipe statements are used in loops, for loop etc..
+
+if(true) {
+    // Compound Statement
+    var a = 10;
+    console.log(a);
+}
+
+// Block scope -> what all variables and fn's can be accessed inside this block is the block scope.
+
+
