a7249e203bf4a65797a3d424a7bd46730aec08fc
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-18 23:43:54 +0530

    docs: Block, Block Scope, Shadowing in JS

--- BlockScopeAndShadowing.ts
+++ BlockScopeAndShadowing.ts
@@ -9,5 +9,56 @@ if(true) {
 }
 
 // Block scope -> what all variables and fn's can be accessed inside this block is the block scope.
+// var is global scoped whereas let & const are block scoped, 
+// which means var can be accessible even outside the block but let/const can't be accessed outside of their block & it will give reference error.
 
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
 
+console.log(a)
+console.log(b)
+console.log(c)
+
+// Shadowing -> means the inner variable has shadowed the value of the outer variable
+
+// 1st example
+var a = 100;
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a) // 10 will be printed here as the inner variable a has shadowed the outer a's value & var is hoisted in global space
+
+// 2nd example
+let b = 100;
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);  // ans => 20 as it's in block scope
+    console.log(c);
+}
+console.log(b) // ans => 100, a new memory space called 'script' is created where this b is hoisted
+
+// 3rd example
+const c = 100;
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);  
+    console.log(c);
+}
+console.log(c)
