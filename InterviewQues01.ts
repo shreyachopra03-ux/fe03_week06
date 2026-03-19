165d3d9d47161df4c8933167a1db1451f395cb56
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-19 20:15:16 +0530

    docs: closures, data privacy using closures, Function constructor, garbage collection

--- InterviewQues01.ts
+++ InterviewQues01.ts
@@ -49,7 +49,7 @@ s();
 // But if have to fix the above problem using var only
 function p() {
     for(var i = 1; i<=5; i++) {
-        function close(p) {
+        function close(p:number): void{
             setTimeout(function() {
                 console.log(p);
             }, p * 1000);
@@ -63,3 +63,69 @@ p();
 // use of double parantheses in js: call a function, then immediately call what it returns.
 
 // Example of data privacy using closures: 
+function counter(){
+    var count = 0;
+    return function incrementCounter(){
+        count++;
+        console.log(count);
+    }
+}
+
+var counter1 = counter();
+counter1();
+counter1();
+
+var counter2 = counter();
+counter2();
+
+// Fn constructor in js
+function Counter(this: any){
+    var count = 0;
+
+    this.incrementCounter = function() {
+        count++;
+        console.log(count);
+    }
+    this.decrementCounter = function() {
+        count--;
+        console.log(count);
+    }
+}
+
+// Disdadvanages of closures
+// - Memory usage
+// - Performance impact
+// - Hard to debug
+// - Code complexity
+// - Delayed garbage collection
+
+// Garbage collector in js: mechanism in JS that automatically frees memory by removing variables/objects that are no longer used.
+
+// Relation between garbage collection, memory leaks and closures
+function a() {
+    var x = 0;
+    return function m(){
+        console.log(x);
+    }
+}
+
+var m = a();
+//.......
+
+y();
+
+// example of smart garbage collection by v8 js engine in chrome
+// Here there's no use of p so it's smart garbage collection
+// Smart garbage collection is when the JavaScript engine automatically removes unused variables from memory, even within closures, and keeps only those variables that are actually being used or referenced.
+
+function z(){
+    var x = 0, p = 10;
+    return function n(){
+        console.log(x)
+    }
+}
+
+var q = z();
+//.....
+
+q();
