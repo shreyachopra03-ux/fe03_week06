32dd1614d432e8ea642fef7c8dcdc47f0c95a7c1
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-21 18:33:13 +0530

    docs: callback function, event listeners, garbage collection and removeEventListeners

--- CallbackFunction/index.js
+++ CallbackFunction/index.js
@@ -14,7 +14,7 @@ x(function y(){
     console.log("y");
 });
   
-// Js is a synchronous and single threaded language
+// Js is a synchronous and single threaded language.
 
 // BLOCKING THE MAIN THREAD
 // Everything inside js is executed through the call stack only and if any operation blocks this call stack then it's called blocking the main thread.
@@ -23,13 +23,25 @@ x(function y(){
 // Callback fn's are always stored in heap memory and pushed into the call stack at the time of their occurence.
 
 // DEEP ABOUUT EVENT LISTENERS
-document.getElementById('btn').addEventListener("click", function xyn(){
+document.getElementById('btn')
+.addEventListener("click", function xyn(){
     console.log("btn clicked");
 });
 
-
 // closures demo with Event Listeners
+// O/p -> btn clicked 1, btn clicked 2 ...
+
+function attachEventListeners(){
+    let count = 0;
+document.getElementById('btn')
+.addEventListener("click", function xyn(){
+    console.log("btn clicked");
+})
+};
 
 // Scope Demo with Event Listeners
 
-// Grabage Collection & removeEventListeners
+// GARBAGE COLLECTION AND removeEventListeners
+// vvv imp interview ques -> why do we need to remove event listeners ?
+// Removing event listeners prevents Memory Leaks by allowing the Garbage Collector to free up RAM. 
+// It also stops Duplicate Execution, ensuring your logic doesn't run multiple times on a single click.
