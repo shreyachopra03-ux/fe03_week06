6e207ea1952b41841a7308cea3d02b7ed3eb19d3
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-21 16:22:24 +0530

    docs: Callback Function

---
+++ CallbackFunction/index.js
@@ -0,0 +1,35 @@
+// What is a callback function in js?
+// Callback Function is a function passed into another function as an argument
+// Callback fn gives us the access to whole async world in a sync single threaded language
+
+// ex
+setTimeout(function () {
+    console.log("timer");
+}, 5000);
+
+function x(y){
+    console.log("x");
+}
+x(function y(){
+    console.log("y");
+});
+  
+// Js is a synchronous and single threaded language
+
+// BLOCKING THE MAIN THREAD
+// Everything inside js is executed through the call stack only and if any operation blocks this call stack then it's called blocking the main thread.
+
+// POWER OF CALLBACK
+// Callback fn's are always stored in heap memory and pushed into the call stack at the time of their occurence.
+
+// DEEP ABOUUT EVENT LISTENERS
+document.getElementById('btn').addEventListener("click", function xyn(){
+    console.log("btn clicked");
+});
+
+
+// closures demo with Event Listeners
+
+// Scope Demo with Event Listeners
+
+// Grabage Collection & removeEventListeners
