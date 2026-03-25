3d6900f728bc950667aed391b399647fed5fbc52
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-25 14:39:48 +0530

    docs: Promises, Callbacks v/s Promises, Importance Of Promises, Promises Object In Browser, Promise Chaining

--- Promises.js
+++ Promises.js
@@ -1,6 +1,6 @@
 // Promises are used to handle async operations in js.
  
-const cart = ["shoes", "lipgloss", "skirts"];
+const cart = ["shoes", "gloss", "skirts"];
 
 // Callbacks
 // Here, we are passing a function.
@@ -9,7 +9,7 @@ createOrder(cart, function (orderId) {
     processToPayment(orderId);
 });
 
-// Promises
+// Promises -> A Promise is an object that represents the eventual completion or failure of an asynchronous operation and its resulting value.
 // Here, we are attaching a function.
 
 const promise = createOrder(cart);
@@ -30,8 +30,17 @@ promise.then(function (orderId) {
 // PromiseState -> It tells in which state the promise is currently, initially it's in pending state only.
 // PromiseResult -> It gives the data that the fetch will get, unless the data has come the value of this remains undefined.
 // There can only be 3 states inside promises: Pending, Fulfilled and Rejected.
-// Promise objects are immutable.
+// Promise objects are immutable, and resolved just once.
 
 const GITHUB_API = " https://api.github.com/users/shreyachopra03-ux"
-
 const user = fetch(GITHUB_API);
+console.log(user);
+
+// Promise Chaining 
+// We always return a promise from a promise when we are chaining it to pass the result to the next .then and hence, avoid the callback hell.
+
+createOrder(cart)
+   .then(orderId => proceedToPyament(orderId))
+   .then(paymentInfo => showOrderSummary(paymentInfo))
+   .then(paymentInfo => updateWalletBalance(paymentInfo))
+
