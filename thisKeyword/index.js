232c8555eaece77def3ff83aabc175885dd3b291
Shreya Chopra <chopra.shreya03@gmail.com>
2026-03-27 13:08:31 +0530

    docs: this Keyword

---
+++ thisKeyword/index.js
@@ -0,0 +1,19 @@
+// this in global space
+
+console.log(this)
+
+// this inside a function
+
+// this in strict mode - (this substitution)
+
+// this value depends on how this is called (window)
+
+// this inside a object's method
+
+// call apply bind methods (sharing methods)
+
+// this inside arrow function
+
+// this inside nested arrow function
+
+// this inside DOM
